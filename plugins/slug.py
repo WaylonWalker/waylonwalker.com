cc2888ee4b6dd610d419504d12aca46846471394
Waylon Walker <quadmx08@gmail.com>
2021-03-31 08:50:00 -0500

    use custom slugger

---
+++ plugins/slug.py
@@ -0,0 +1,24 @@
+"""Flat Slug Plugin
+
+Creates a slug in article.metadata if missing based on filename.
+"""
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from markata.hookspec import hook_impl
+
+if TYPE_CHECKING:
+    from markata import Markata
+
+
+@hook_impl(tryfirst=True)
+def render(markata: "Markata") -> None:
+    for article in markata.iter_articles(description="creating slugs"):
+        try:
+            article["slug"] = article.metadata["slug"]
+        except KeyError:
+            article["slug"] = (
+                (str(Path(article["path"]).parent / Path(article["path"]).stem))
+                .replace("pages/", "")
+                .replace("blog/", "")
+            )
