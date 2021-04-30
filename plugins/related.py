192e11e32d0f955fc9f3b552889b48b4337437ab
Waylon Walker <quadmx08@gmail.com>
2021-04-29 23:28:48 -0500

    FEAT release related plugin

---
+++ plugins/related.py
@@ -0,0 +1,39 @@
+from typing import Union, List
+from markata.hookspec import hook_impl
+from string import Template
+from pathlib import Path
+from more_itertools import unique_everseen
+
+
+@hook_impl
+def render(markata):
+    published_articles = [a for a in markata.articles if a["status"] == "published"]
+    published_articles.sort(key=lambda x: x["date"])
+    for article in markata.iter_articles("related"):
+        tags = article["tags"]
+        try:
+            index = published_articles.index(article)
+            articles = published_articles[index:] + list(
+                reversed(published_articles[:index])
+            )
+            tagged_articles = [
+                a
+                for a in articles
+                if set(article["tags"]) & set(a["tags"]) and a != article
+            ]
+            related_articles = list(unique_everseen([*tagged_articles, *articles]))[:3]
+            # article["related"] = related_articles
+            article.content = (
+                article.content + "---\n## Check Out These Related Posts\n\n"
+            )
+            article.content = article.content + "\n\n".join(
+                [
+                    "https://waylonwalker.com/" + a["slug"] + "/"
+                    for a in related_articles
+                ]
+            )
+
+        except ValueError:
+
+            # article is not in published articles
+            pass
