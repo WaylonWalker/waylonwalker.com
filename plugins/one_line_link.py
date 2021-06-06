41a16b5d75d2eaef98663207e7d46164525b0c91
Waylon Walker <quadmx08@gmail.com>
2021-06-06 08:15:04 -0500

    FIX one_line_link

--- plugins/one_line_link.py
+++ plugins/one_line_link.py
@@ -28,22 +28,22 @@ def expand_line(line):
     Todo: better error message over base exception
     Todo: try except decorator?
     """
-    try:
-        r = RE_ONE_LINE.match(line)
-        if r and " " not in line:
-            return get_one_line_link(line)
-        r = RE_TWEET.match(line)
-        if r and " " not in line:
-            html = API.GetStatusOembed(url=line)["html"]
-            return html
+    # try:
+    r = RE_ONE_LINE.match(line)
+    if r and " " not in line:
+        return get_one_line_link(line)
+    r = RE_TWEET.match(line)
+    if r and " " not in line:
+        html = API.GetStatusOembed(url=line)["html"]
+        return html
 
-        return line
-    except BaseException as e:
-        import warnings
+    return line
+    # except BaseException as e:
+    #     import warnings
 
-        warnings.warn(f"OneLineLink threw an error\n {e}")
+    #     warnings.warn(f"OneLineLink threw an error\n {e}")
 
-        return line
+    #     return line
 
 
 class OneLineLinkError(KeyError):
@@ -60,24 +60,46 @@ def get_one_line_link(link):
         url = soup.find("meta", attrs={"name": "og:url"})["content"]
     except KeyError:
         url = link
+    except TypeError:
+        url = link
 
     try:
         sm_img = soup.find("meta", attrs={"name": "og:sm_image"})["content"]
     except KeyError:
-        sm_img = soup.find("meta", attrs={"name": "og:image"})["content"]
-    except KeyError:
-        raise OneLineLinkError(f"could not find sm_img on {link}")
+        try:
+            sm_img = soup.find("meta", attrs={"name": "og:image"})["content"]
+        except KeyError:
+            raise OneLineLinkError(f"could not find sm_img on {link}")
+    except TypeError:
+        try:
+            sm_img = soup.find("meta", attrs={"name": "og:image"})["content"]
+        except KeyError:
+            raise OneLineLinkError(f"could not find sm_img on {link}")
+        except TypeError:
+            raise OneLineLinkError(f"could not find sm_img on {link}")
 
     try:
         title = soup.find("title").text
     except KeyError:
-        title = soup.find("meta", attrs={"name": "og:title"})["content"]
-    except KeyError:
-        raise OneLineLinkError(f"could not find title on {link}")
+        try:
+            title = soup.find("meta", attrs={"name": "og:title"})["content"]
+        except KeyError:
+            raise OneLineLinkError(f"could not find title on {link}")
+        except TypeError:
+            raise OneLineLinkError(f"could not find title on {link}")
+    except TypeError:
+        try:
+            title = soup.find("meta", attrs={"name": "og:title"})["content"]
+        except KeyError:
+            raise OneLineLinkError(f"could not find title on {link}")
+        except TypeError:
+            raise OneLineLinkError(f"could not find title on {link}")
 
     try:
         description = soup.find("meta", attrs={"name": "og:description"})["content"]
     except KeyError:
+        description = soup.text[:120]
+    except TypeError:
         description = soup.text[:120]
 
     description = description.replace("\n", " ").replace("\r", "")
@@ -111,8 +133,9 @@ def expand_article(content):
 
 
 @hook_impl
-def render(markata):
-    for article in markata.iter_articles("Expand One Line Links"):
+def pre_render(markata):
+    # for article in markata.iter_articles("Expand One Line Links"):
+    for article in markata.articles:
 
         html_key = markata.make_hash(
             "one_line_link", "render", "html", article["content_hash"]
