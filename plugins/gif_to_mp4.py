ba380f45c371248fbc91442096e33f0c3c526e0e
Waylon Walker <quadmx08@gmail.com>
2021-06-09 08:37:39 -0500

    ADD gif_to_mp4 plugin

---
+++ plugins/gif_to_mp4.py
@@ -0,0 +1,75 @@
+from markata.hookspec import hook_impl
+from bs4 import BeautifulSoup
+import re
+import requests
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bs4.element import Tag
+
+
+def url_exists(url: str) -> bool:
+    r = requests.head(url)
+    return r.status_code == 200
+
+
+def render_mp4(gif: "Tag") -> "Tag":
+    "Render a video tag given a validatated image tag."
+
+    webm_src = re.sub(".gif$", ".webm", gif.attrs["src"])
+    mp4_src = re.sub(".gif$", ".mp4", gif.attrs["src"])
+
+    if not url_exists(webm_src):
+        return gif
+
+    if not url_exists(mp4_src):
+        return gif
+
+    html = f"""
+        <video controls muted autoplay playsinline loop=true width="100%">
+            <source src="{webm_src}"
+                    type="video/webm">
+            <source src="{mp4_src}"
+                    type="video/mp4">
+            Sorry, your browser doesn't support embedded videos.
+        </video>
+
+        <div class='speed-control'>
+            <button onclick="change_speed(.25)" >
+                speed up
+            </button>
+            <button onclick="change_speed(-.25)" >
+                slow down
+            </button>
+        </div>
+    """
+    return BeautifulSoup(html, "html.parser")
+
+
+def is_valid_gif(gif: "Tag") -> bool:
+    src = gif.attrs["src"]
+    return src.startswith("https://images.waylonwalker.com") and src.endswith(".gif")
+
+
+def swap_gif(gif: "Tag") -> "Tag":
+    if is_valid_gif(gif):
+        return render_mp4(gif)
+    else:
+        return gif
+
+
+def swap_gifs(soup: BeautifulSoup) -> None:
+    """Finds gifs in an articles soup and swaps for mp4s"""
+    gifs = soup.find_all("img")
+    for gif in gifs:
+        gif.replace_with(swap_gif(gif))
+
+
+@hook_impl
+def post_render(markata):
+    "Hook to replace gifs on images.waylonwalker.com with mp4's if they exist"
+    for article in markata.articles:
+        soup = BeautifulSoup(article.html, "html.parser")
+        swap_gifs(soup)
+        article.html = soup.prettify()
