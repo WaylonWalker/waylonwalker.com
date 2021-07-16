d1a2b54ca7148db543a9fdabe3efd18677ca4f4e
Waylon Walker <waylon@waylonwalker.com>
2021-07-16 08:17:06 -0500

    add rss_tag

---
+++ plugins/rss_tag.py
@@ -0,0 +1,65 @@
+"""tagged rss plugin"""
+from pathlib import Path
+from typing import TYPE_CHECKING, cast, Dict
+
+import pytz
+from feedgen.feed import FeedGenerator
+from more_itertools import flatten
+
+from markata import Markata
+from markata.hookspec import hook_impl
+
+if TYPE_CHECKING:
+    from markata import Markata
+
+class MarkataRss(Markata):
+    fg: "FeedGenerator"
+    rss: str
+    rss_tags: Dict
+
+
+@hook_impl(trylast=True)
+def render(markata: "MarkataRss") -> None:
+    status = "published"
+    all_posts = reversed(sorted(markata.articles, key=lambda x: x["date"]))
+    posts = [post for post in all_posts if post["status"] == status]
+    tags = list(set(flatten([a['tags'] for a in markata.articles])))
+    tagged_posts = {tag: [post for post in all_posts if tag in post["tags"]] for tag in tags} 
+
+
+    markata.rss_tags = {tag: make_rss(markata, posts, tag) for tag, posts in tagged_posts.items()}
+    
+
+
+def make_rss(markata: MarkataRss, posts: list, tag:str) -> FeedGenerator:
+    fg = FeedGenerator()
+    fg.id(markata.url + f"/{tag}-rss.xml")
+    fg.title(f'{markata.title} - {tag} posts')
+    fg.author({"name": markata.author_name, "email": markata.author_email})
+    fg.link(href=markata.url, rel="alternate")
+    fg.logo(markata.icon)
+    fg.subtitle(f'{markata.rsarkatas_description} - {tag} posts')
+    fg.link(href=markata.url + f"/{tag}-rss.xml", rel="self")
+    fg.language(markata.lang)
+
+
+    for article in posts:
+        fe = fg.add_entry()
+        fe.id(markata.url + "/" + article["slug"])
+        fe.title(article.metadata["title"])
+        fe.published(article.metadata["datetime"])
+        fe.description(article.metadata["description"])
+        fe.summary(article.metadata["long_description"])
+        fe.link(href=markata.url + "/" + article["slug"])
+        fe.content(article.article_html.translate(dict.fromkeys(range(32))))
+
+    rss = fg.rss_str(pretty=True)
+    return rss
+
+
+@hook_impl
+def save(markata: "MarkataRss") -> None:
+    output_dir = Path(markata.output_dir)
+
+    for tag, rss in markata.rss_tags.items():
+        rss.rss_file(str(output_dir /tag/ "rss.xml"))
