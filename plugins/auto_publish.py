1dcf4d5585dd2e0f6b2470054d9f85134db15882
Waylon S. Walker <waylon@waylonwalker.com>
2021-12-30 18:59:23 -0600

    auto-publish those tils

---
+++ plugins/auto_publish.py
@@ -0,0 +1,13 @@
+from markata.hookspec import hook_impl
+
+
+@hook_impl(trylast=True)
+def load(markata):
+    """
+    change status to published for template
+    """
+    config = markata.get_plugin_config(__file__)
+    for article in markata.articles:
+        for filter in config["filters"].values():
+            for article in markata.filter(filter):
+                article["status"] = "published"
