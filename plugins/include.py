d6668a5a318777aff3a9972bd420e3aa383cf6c0
Waylon S. Walker <waylon@waylonwalker.com>
2022-01-04 22:15:12 -0600

    add include plugin

---
+++ plugins/include.py
@@ -0,0 +1,20 @@
+from markata.hookspec import hook_impl
+
+
+def expand_content(content: str, markata):
+    slugs = []
+    for line in content.split("\n"):
+        if line.startswith("%%include"):
+            slugs.append(line.replace("%%include ", ""))
+    for slug in slugs:
+        include_content = markata.filter(f'slug=="{slug}"')[0].content
+        content = content.replace(f"%%include {slug}", include_content)
+    return content
+
+
+@hook_impl(trylast=True)
+def pre_render(markata):
+    for article in markata.articles:
+
+        if "%%include" in article.content:
+            article.content = expand_content(article.content, markata)
