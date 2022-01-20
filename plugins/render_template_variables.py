2937f34bc7503ab9c908e48d9655658740aa14e6
Waylon S. Walker <waylon@waylonwalker.com>
2022-01-19 21:51:18 -0600

    convert all dates to datetime

--- plugins/render_template_variables.py
+++ plugins/render_template_variables.py
@@ -8,7 +8,12 @@ from markata.hookspec import hook_impl, register_attr
 def pre_render(markata) -> None:
     for article in markata.iter_articles("rendering markdown"):
         try:
+            article["date"] = datetime.datetime.today().min
             article["year"] = article["date"].year
         except KeyError:
             article["year"] = datetime.datetime.today().year
             article["date"] = datetime.datetime.today().min
+        if isinstance(article["date"], datetime.date):
+            article["date"] = datetime.datetime.combine(
+                article["date"], datetime.datetime.min.time()
+            )
