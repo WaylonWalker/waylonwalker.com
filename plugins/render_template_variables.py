464bb29c3c14a2bb9a00ef59feddb1cc8ee1c141
Waylon S. Walker <waylon@waylonwalker.com>
2022-01-20 08:26:54 -0600

    fix datetimes

--- plugins/render_template_variables.py
+++ plugins/render_template_variables.py
@@ -1,5 +1,6 @@
 import datetime
 
+import pytz
 from markata.hookspec import hook_impl, register_attr
 
 
@@ -7,14 +8,33 @@ from markata.hookspec import hook_impl, register_attr
 @register_attr("articles")
 def pre_render(markata) -> None:
     for article in markata.iter_articles("rendering markdown"):
-        try:
+
+        if "date" not in article.keys():
             article["date"] = datetime.datetime.today().min
+
+        if article["date"] is None:
+            article["date"] = datetime.datetime.today().min
+
+        if "datetime" not in article.keys():
+            # all dates get cohersed to datetime later
+            article["datetime"] = article["date"]
+
+        if article["datetime"] is None:
+            article["datetime"] = datetime.datetime.now().min
+
+        if isinstance(article["date"], datetime.datetime):
+            article["date"] = article["date"].date()
+
+        if isinstance(article["datetime"], datetime.date):
+            article["datetime"] = datetime.datetime.combine(
+                article["datetime"], datetime.datetime.min.time()
+            )
+
+        if article["datetime"].tzinfo is None:
+            article["datetime"] = article["datetime"].replace(tzinfo=pytz.utc)
+
+        try:
             article["year"] = article["date"].year
         except KeyError:
             article["year"] = datetime.datetime.today().year
             article["date"] = datetime.datetime.today().min
-        if isinstance(article["date"], datetime.datetime):
-            # article["date"] = datetime.datetime.combine(
-            #     article["date"], datetime.datetime.min.time()
-            # )
-            article["date"] = article["date"].date()
