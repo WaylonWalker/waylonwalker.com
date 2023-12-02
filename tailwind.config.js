5f4cb1cbe88ccec9595836c3826092c1f2875503
Waylon S. Walker <waylon@waylonwalker.com>
2023-12-02 11:50:57 -0600

    get tailwinded

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["templates/**/*.{html,js}"],
+  plugins: [require("@tailwindcss/typography")],
+  purge: {
+    enabled: true,
+  },
+  theme: {
+    extend: {
+      saturate: {
+        25: ".25",
+        75: ".75",
+      },
+      screens: {
+        print: { raw: "print" },
+      },
+      width: {
+        128: "32rem",
+      },
+      boxShadow: {
+        xlc: "0 0 60px 15px rgba(0, 0, 0, 0.3)",
+      },
+    },
+  },
+};
