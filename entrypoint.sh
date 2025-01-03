c27cb3ebc14fab317214b98bc14c362280341c4e
Waylon S. Walker <waylon@waylonwalker>
2025-01-03 10:22:50 -0600

    markata.json is too large for cloudflare pages

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ git config --global --add safe.directory /site/waylonwalker.com
 git pull
 mkdir -p markout
 markata build
+rm markout/markata.json
 wrangler pages deploy markout --project-name dev-waylonwalker-com --branch main --commit-message "deploy main $(date)"
 # wrangler pages deploy markout --project-name reader-waylonwalker-com --branch markout
