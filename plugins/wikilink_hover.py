102c3a96b1897a268178b3249a5ca51485462a85
Waylon S. Walker <waylon@waylonwalker>
2025-01-15 10:43:30 -0600

    fix posibility of double slashes from wiki link hover

--- plugins/wikilink_hover.py
+++ plugins/wikilink_hover.py
@@ -7,7 +7,7 @@ if TYPE_CHECKING:
     pass
 
 external_svg = """
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4 inline align-middle">
+<svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4 inline align-middle">
   <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
 </svg>
 """
@@ -26,7 +26,7 @@ def hover_links(soup):
             parent.parent["class"] = parent.parent.get("class", []) + [
                 "hover:z-20",
             ]
-        href = link.attrs.get("href")
+        href = link.attrs.get("href").lstrip("/")
 
         prefix = ""
         boost = ""
@@ -60,7 +60,7 @@ def hover_links(soup):
     <span class="z-10 group group-hover:z-20 relative inline-block">
         <a class="wikilink text-pink-500 hover:underline" href="{href}" title="{title}"{boost}>{prefix} {title}</a>
         <button class="ml-2 text-pink-500 hover:underline focus:outline-none" aria-label="Preview">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            <svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="h-5 w-5 inline align-middle">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
